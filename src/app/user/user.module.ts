4755241d7eeec1a6303cf6cb0ae67daa61f63cae
Janku13 <mohammed.mmaljoke@gmail.com>
2022-08-30 22:59:59 -0300

    starting register validation

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -10,11 +13,14 @@ import { SharedModule } from '../shared/shared.module';
   declarations: [
 
   
-    AuthModalComponent
+    AuthModalComponent,
+          LoginComponent,
+          RegisterComponent
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ],
   exports: [
     AuthModalComponent
